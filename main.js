b4f81f7d226092ec2ae90a70ce97241b9d8530b1
Dennis Duka <38925592+GreyTeddy@users.noreply.github.com>
2024-05-07 22:26:13 +0100

    pages and groups can now be added more easily

--- main.js
+++ main.js
@@ -1,20 +1,48 @@
 const form = document.getElementById("search_term_form");
 const input = document.getElementById("search_text_input")
-const general_search_pages = [
-  "https://www.google.com/search?q={{}}",
-  "https://uk.search.yahoo.com/search?p={{}}"
+const radioButtonDiv = document.getElementById("radio_inputs_to_be_populated")
+const pages = [
+  {
+    "name": "General",
+    "pages": [
+      "https://www.google.com/search?q={{}}",
+      "https://uk.search.yahoo.com/search?p={{}}"
+    ],
+  },
+  {
+    "name": "Stocks",
+    "pages": [
+      "https://uk.finance.yahoo.com/quote/{{}}"
+    ]
+  }
 ]
+
+// create_radio_buttons
+const radio_buttons = []
+for (let pagesIndex = 0; pagesIndex < pages.length; pagesIndex++) {
+  const element = pages[pagesIndex];
+  radio_buttons.push(`
+    <input type="radio" id="${element.name}_radio_button" name="search_group" value="${element.name}"/>
+    <label for="${element.name}_radio_button">${element.name}</label>
+  `)
+}
+
+radioButtonDiv.innerHTML = radio_buttons.join(" ")
+radioButtonDiv.firstElementChild.checked = true
 input.focus()
-form.addEventListener("submit", async  (event) => {
+form.addEventListener("submit", async (event) => {
   // console.log(event);
   const tabs = []
   const input = document.querySelector("input");
   if (input.value.length == 0) return;
 
-  const pages = general_search_pages;
-  for (let pagesIndex = 0; pagesIndex < pages.length; pagesIndex++) {
-    const element = pages[pagesIndex];
-    tabs.push(await chrome.tabs.create({url:element.replaceAll("{{}}",encodeURIComponent(input.value))}))
+  const buttonData = new FormData(form);
+  const group_name = buttonData.get("search_group");
+  const group_pages = pages.filter((e)=>e.name == group_name)[0].pages;
+
+  for (let pagesIndex = 0; pagesIndex < group_pages.length; pagesIndex++) {
+    const page_URL = group_pages[pagesIndex];
+    tabs.push(await chrome.tabs.create({url:page_URL.replaceAll("{{}}",encodeURIComponent(input.value))}))
   }
 
   const tabIds = tabs.map(({id}) => id);
