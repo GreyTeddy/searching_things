5aff141c186b2c6183cda6014f06e4149145e633
Dennis Duka <38925592+GreyTeddy@users.noreply.github.com>
2024-05-07 21:41:38 +0100

    better way to store pages to be used

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
-const button = document.getElementById("search_term_form");
-button.addEventListener("submit", async  (event) => {
+const form = document.getElementById("search_term_form");
+const input = document.getElementById("search_text_input")
+const general_search_pages = [
+  "https://www.google.com/search?q={{}}",
+  "https://uk.search.yahoo.com/search?p={{}}"
+]
+input.focus()
+form.addEventListener("submit", async  (event) => {
   // console.log(event);
   const tabs = []
   const input = document.querySelector("input");
   if (input.value.length == 0) return;
-  tabs.push(await chrome.tabs.create({url:`https://www.google.com/search?q=${encodeURIComponent(input.value)}`}));
-  tabs.push(await chrome.tabs.create({url:`https://uk.search.yahoo.com/search?p=${encodeURIComponent(input.value)}`}));
+
+  const pages = general_search_pages;
+  for (let pagesIndex = 0; pagesIndex < pages.length; pagesIndex++) {
+    const element = pages[pagesIndex];
+    tabs.push(await chrome.tabs.create({url:element.replaceAll("{{}}",encodeURIComponent(input.value))}))
+  }
 
   const tabIds = tabs.map(({id}) => id);
   if (tabIds.length == 0){
