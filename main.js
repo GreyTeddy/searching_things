004e8c55eb6b31e01af51d7a0a5d9be1cb783f10
Dennis Duka <38925592+GreyTeddy@users.noreply.github.com>
2024-05-09 23:29:35 +0100

    added ability to add and remove groups and pages

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const form = document.getElementById("search_term_form");
 const input = document.getElementById("search_text_input")
 const pagesGroupSelect = document.getElementById("pages_group_select")
-const pages = [
+const defaultGroupsAndPages = [
   {
     "name": "General",
     "pages": [
@@ -24,16 +24,29 @@ const pages = [
   }
 ]
 
+const groupAndPages = (await chrome.storage.sync.get())["groupsAndPages"]
+if (groupAndPages == undefined) {
+  chrome.storage.sync.set(
+    { groupsAndPages: defaultGroupsAndPages },
+    async () => {
+      console.log(await chrome.storage.sync.get())
+      console.log("defaults set")
+    }
+  );
+}
+
 const pages_group_options = []
-for (let pagesIndex = 0; pagesIndex < pages.length; pagesIndex++) {
-  const element = pages[pagesIndex];
+for (let pagesIndex = 0; pagesIndex < groupAndPages.length; pagesIndex++) {
+  const element = groupAndPages[pagesIndex];
   pages_group_options.push(`
     <option id="${element.name}_option" name="${element.name}" value="${element.name}">${element.name}</option>
   `)
 }
 
 pagesGroupSelect.innerHTML = pages_group_options.join(" ")
-pagesGroupSelect.firstElementChild.checked = true
+if (pagesGroupSelect.firstElementChild) {
+  pagesGroupSelect.firstElementChild.checked = true
+}
 input.focus()
 form.addEventListener("submit", async (event) => {
   const tabs = []
@@ -42,7 +55,7 @@ form.addEventListener("submit", async (event) => {
 
   const buttonData = new FormData(form);
   const group_name = buttonData.get("pages_group_select");
-  const group_pages = pages.filter((e) => e.name == group_name)[0].pages;
+  const group_pages = groupAndPages.filter((e) => e.name == group_name)[0].pages;
 
   for (let pagesIndex = 0; pagesIndex < group_pages.length; pagesIndex++) {
     const page_URL = group_pages[pagesIndex];
---
+++ options.js
@@ -0,0 +1,138 @@
+const clearGroups = async () => {
+  await chrome.storage.sync.set({ groupsAndPages: [] });
+  console.log(await chrome.storage.sync.get())
+  location.reload()
+};
+
+document.getElementById('clear_groups').addEventListener('click', clearGroups);
+
+addPage = async (group_name, page_URL) => {
+  let groupsAndPages = (await chrome.storage.sync.get())["groupsAndPages"]
+  if (!groupsAndPages || groupsAndPages.map(e => e.name).indexOf(group_name) == -1) {
+    return { success: false, reason: "Group Name does not exist" }
+  }
+  const index_of_group = groupsAndPages.map(e => e.name).indexOf(group_name)
+  groupsAndPages[index_of_group].pages.push(page_URL)
+  await chrome.storage.sync.set(
+    { groupsAndPages: groupsAndPages },
+    async () => {
+      console.log(await chrome.storage.sync.get())
+    }
+  );
+  location.reload()
+  return { success: true }
+}
+
+removePage = async (group_name, page_URL) => {
+  let groupsAndPages = (await chrome.storage.sync.get())["groupsAndPages"]
+  if (!groupsAndPages || groupsAndPages.map(e => e.name).indexOf(group_name) == -1) {
+    return { success: false, reason: "Group Name does not exist" }
+  }
+  const index_of_group = groupsAndPages.map(e => e.name).indexOf(group_name)
+  const index_of_page = groupsAndPages[index_of_group].pages.indexOf(page_URL)
+  if (index_of_page == -1) {
+    return { success: false, reason: `Page URL ${page_URL} is not part of the group: ${group_name}` }
+  }
+  groupsAndPages[index_of_group].pages.splice(index_of_page, 1)
+  await chrome.storage.sync.set(
+    { groupsAndPages: groupsAndPages },
+    async () => {
+      console.log(await chrome.storage.sync.get())
+      console.log("defaults set")
+    }
+  );
+  location.reload()
+  return { success: true }
+}
+
+removeGroup = async (group_name) => {
+  let groupsAndPages = (await chrome.storage.sync.get())["groupsAndPages"]
+  if (!groupsAndPages || groupsAndPages.map(e => e.name).indexOf(group_name) == -1) {
+    return { success: false, reason: "Group Name does not exist" }
+  }
+  const index_of_group = groupsAndPages.map(e => e.name).indexOf(group_name)
+  groupsAndPages.splice(index_of_group, 1)
+  await chrome.storage.sync.set(
+    { groupsAndPages: groupsAndPages },
+    async () => {
+      console.log(await chrome.storage.sync.get())
+    }
+  );
+  location.reload()
+  return { success: true }
+}
+
+addGroup = async (group_name) => {
+  let groupsAndPages = (await chrome.storage.sync.get())["groupsAndPages"]
+  if (!groupsAndPages || groupsAndPages.map(e => e.name).indexOf(group_name) != -1) {
+    return { success: false, reason: "Group name already exists" }
+  }
+  groupsAndPages.push({ name: group_name, pages: [] })
+  await chrome.storage.sync.set(
+    { groupsAndPages: groupsAndPages },
+    async () => {
+      console.log(await chrome.storage.sync.get())
+    }
+  );
+  location.reload()
+  return { success: true }
+}
+const handleButtonClick = async (e) => {
+  if (e.target.classList.contains("remove-group-button")) {
+    console.log(await removeGroup(e.target.nextSibling.data.trim()))
+  } else if (e.target.classList.contains("remove-page-button")) {
+    console.log(await removePage(e.target.parentElement.parentElement.previousElementSibling.nextSibling.data.trim(), e.target.nextSibling.data.trim()))
+  }
+}
+const handleFormSubmit = async (e) => {
+  console.log(e)
+  if (e.target.classList.contains("add-page-form")) {
+    console.log(await addPage(e.target.parentElement.previousSibling.textContent.trim(), e.target.firstChild.value))
+  } else if (e.target.classList.contains("add-group-form")) {
+    console.log(e.target.firstChild.value)
+    // return
+    console.log(await addGroup(e.target.firstChild.value))
+  }
+}
+const showPages = () => {
+  chrome.storage.sync.get(
+    "groupsAndPages",
+    (items) => {
+      if (!items.groupsAndPages) {
+        document.getElementById('groups_and_pages').innerHTML = `
+          <p>No Settings Set</p>
+        `;
+        return
+      }
+      let result = ""
+      for (let index = 0; index < items.groupsAndPages.length; index++) {
+        const group = items.groupsAndPages[index];
+        result += `
+        <button class="remove-group-button groups-and-pages-button" style='margin-right: 5px'>-</button>
+        ${group.name}
+        <div style="margin-left: 20px">
+        ${group.pages.map(page_URL =>
+          `<div><button class="remove-page-button groups-and-pages-button" style="margin-right: 5px">-</button>${page_URL}</div>`
+        ).reduce((e, i) => e + i, "")}
+        <form class="add-page-form groups-and-pages-form"><input class="test" placeholder="Add Page"></input></form>
+        </div>
+        `
+      }
+      result += `<form class="add-group-form groups-and-pages-form"><input class="test" placeholder="Add Group"></input></form>`
+      document.getElementById('groups_and_pages').innerHTML = result;
+      let groupsAndPagesButtons = document.getElementsByClassName('groups-and-pages-button');
+      for (let index = 0; index < groupsAndPagesButtons.length; index++) {
+        const element = groupsAndPagesButtons.item(index);
+        element.addEventListener('click', handleButtonClick)
+      }
+      let groupsAndPagesForms = document.getElementsByClassName('groups-and-pages-form');
+      for (let index = 0; index < groupsAndPagesForms.length; index++) {
+        const element = groupsAndPagesForms.item(index);
+        element.addEventListener('submit', handleFormSubmit)
+      }
+    }
+  );
+};
+
+// .forEach(e=>e.addEventListener('click', test))
+document.addEventListener('DOMContentLoaded', showPages);
